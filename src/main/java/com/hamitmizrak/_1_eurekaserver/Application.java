6e6f15c0c0f03819e292be8daca79f44dbb7c576
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-11 15:32:17 +0300

    Application

--- src/main/java/com/hamitmizrak/_1_eurekaserver/Application.java
+++ src/main/java/com/hamitmizrak/_1_eurekaserver/Application.java
@@ -2,12 +2,15 @@ package com.hamitmizrak._1_eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// Eureka Server(Active)
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+    } //end psvm
 
-}
+} //end Application
