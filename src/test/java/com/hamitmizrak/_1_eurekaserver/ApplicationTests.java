886ba7d5bc1c98368fab89ffd262ac69db3b5072
Hamit Mizrak <hamitmizrak@gmail.com>
2024-11-11 15:14:28 +0300

    Initial commit

---
+++ src/test/java/com/hamitmizrak/_1_eurekaserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak._1_eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
